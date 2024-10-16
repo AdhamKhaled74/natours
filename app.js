79f191a737504d371cd3de769cc88c0346eb8949
AdhamKhaled74 <kadham67153@gmail.com>
2024-10-16 19:24:48 +0300

    improved stripe implementation

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ const userRouter = require(`./routes/userRoutes.js`);
 const reviewRouter = require(`./routes/reviewRoutes.js`);
 const viewRouter = require(`./routes/viewRoutes.js`);
 const bookingRouter = require(`./routes/bookingRoutes.js`);
+// eslint-disable-next-line import/extensions
+const bookingController = require('./controllers/bookingController.js');
 
 const app = express();
 
@@ -98,8 +100,13 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
-app.use('/api', limiter);
 
+app.use('/api', limiter);
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 app.use(compression());
 // Body parser , Reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -17,7 +17,7 @@ router.get('/login', viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
 router.get(
   '/my-tours',
-  BookingController.createBookingCheckout,
+  // BookingController.createBookingCheckout,
   authController.protect,
   viewsController.getMyTours
 );
