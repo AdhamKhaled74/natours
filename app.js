b8ddab5cf549287133d5799f0bd96926fc4b43de
AdhamKhaled74 <kadham67153@gmail.com>
2024-10-18 23:42:45 +0300

    vercel deployment again

--- app.js
+++ app.js
@@ -1,169 +1,134 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const dotenv = require('dotenv');
 const compression = require('compression');
-// eslint-disable-next-line import/extensions
-const cookieParser = require('cookie-parser');
+
+dotenv.config({ path: './config.env' });
 const cors = require('cors');
-// eslint-disable-next-line import/extensions
-const AppError = require('./utils/appError.js');
-// eslint-disable-next-line import/extensions
-const globalErrorHandler = require('./controllers/errorController.js');
 
-// eslint-disable-next-line import/extensions
-const bookingController = require('./controllers/bookingController.js');
+// const AppError = require('./utils/appError');
+const cookieParser = require('cookie-parser');
+const errorController = require('./controllers/errorController');
 
-const tourRouter = require(`./routes/tourRoutes.js`);
-const userRouter = require(`./routes/userRoutes.js`);
-const reviewRouter = require(`./routes/reviewRoutes.js`);
-const viewRouter = require(`./routes/viewRoutes.js`);
-const bookingRouter = require(`./routes/bookingRoutes.js`);
-// eslint-disable-next-line import/extensions
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+const viewRouter = require('./routes/viewRoutes');
+const { webhookCheckout } = require('./controllers/bookingController');
 
 const app = express();
-
 // app.enable('trust proxy');
+app.use(cors()); // cross origin resource sharing [allow everyone]
+// app.use(cors({origin:'https://www.natours.com'})) [only allow usin this domain]
+// simple reqs => get , post
+// non simple reqs => put patch delete or reqs that has cookies
+app.options('*', cors());
+// app.options('api/v1/tours/:id', cors()); // and again we can allow this non-simpeo for spacific route
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// GLOBAL MIDDLEWARES
-// Implement CORS
-app.use(cors());
-// Serving static files
-// app.use(express.static(`${__dirname}/public`)); the same with line 66
-app.use(express.static(path.join(__dirname, 'public')));
+//Global Middleware
+app.use(express.static(path.join(__dirname, 'public'))); // Serving static files
+// 1) set Security HTTP headers
 
-// Set security HTTP Headers
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'", 'data:', 'blob:', 'https:', 'ws:'],
-        baseUri: ["'self'"],
-        fontSrc: ["'self'", 'https:', 'data:'],
-        scriptSrc: [
-          "'self'",
-          'https:',
-          'http:',
-          'blob:',
-          'https://*.mapbox.com',
-          'https://js.stripe.com',
-          'https://m.stripe.network',
-          'https://*.cloudflare.com',
-          'https://vercel.live'
-        ],
-        frameSrc: ["'self'", 'https://js.stripe.com'],
-        objectSrc: ["'none'"],
-        styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
-        workerSrc: [
-          "'self'",
-          'data:',
-          'blob:',
-          'https://*.tiles.mapbox.com',
-          'https://api.mapbox.com',
-          'https://events.mapbox.com',
-          'https://m.stripe.network',
-          'https://vercel.live'
-        ],
-        childSrc: ["'self'", 'blob:'],
-        imgSrc: ["'self'", 'data:', 'blob:'],
-        formAction: ["'self'"],
-        connectSrc: [
-          "'self'",
-          "'unsafe-inline'",
-          'data:',
-          'blob:',
-          'https://*.stripe.com',
-          'https://*.mapbox.com',
-          'https://*.cloudflare.com/',
-          'https://bundle.js:*',
-          'ws://localhost:*/'
-        ],
-        upgradeInsecureRequests: []
-      }
-    }
-  })
-);
+// our app.use take a function not a call of function but this call will return a function
+app.use(helmet());
 
-// Development Logging
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-// Middleware function to limit num of requests
+// 2) set Limite reqs from same API
 const limiter = rateLimit({
   max: 100,
-  windowMs: 60 * 60 * 1000,
+  windowMs: 60 * 60 * 1000, // this means 100 req per one hour for the same ip
   message: 'Too many requests from this IP, please try again in an hour!'
 });
-
 app.use('/api', limiter);
+
+// Middleware
+// Builtin Middleware
+
+// Stripe webhook route
 app.use(
   '/webhook-checkout',
   express.raw({ type: 'application/json' }),
-  bookingController.webhookCheckout
-);
-app.use(compression());
-// Body parser , Reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
-// Parse Cookie header and populate req.cookies
-app.use(cookieParser());
-// parsing form data
-app.use(
-  express.urlencoded({
-    extended: true,
-    limit: '10kb'
-  })
-);
+  webhookCheckout
+); // we want it not in json but in a row formate
+
+app.use(express.json({ limit: '10kb' })); // Body parser, reading data from body into req.body
+
 // Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
+app.use(mongoSanitize()); // this will filter any NoSQL quear
+
 // Data sanitization against XSS
-app.use(xss());
+app.use(xss()); // this will clean the input from any malicious html code
 
 // Prevent parameter pollution
 app.use(
   hpp({
     whitelist: [
       'duration',
-      'ratingsQuantity',
       'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
       'difficulty',
-      'price',
-      'maxGroupSize'
+      'price'
     ]
   })
 );
 
-// Routes
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '10kb'
+  })
+); // parsing form data
+app.use(cookieParser()); // Parse Cookie header and populate req.cookies
 
+app.use((req, res, next) => {
+  // Test Middelware
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers)
+  next();
+});
+
+app.use(compression());
+
+// DEVELOPMENT logging
+// 3rd party Middleware for debuging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Template Routes
 app.use('/', viewRouter);
+
+// RESTfull Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
-app.use('/api/v1/bookings', bookingRouter);
+app.use('/api/v1/booking', bookingRouter);
 
-// app.use((req, res, next) => {
-//   console.log('Hello from the middleware');
-//   next();
+// Handling unhandeld Routes [Operational Error]
+// app.all('*', (req, res, next) => {
+//   // res.status(404).json({
+//   //   status: 'fail',
+//   //   message:`Cant find ${req.originalUrl} on this server!`
+//   // })
+
+//   // const err = new Error(`Cant fint ${req.originalUrl} on this server!`);
+//   // err.status = 'fail';
+//   // err.statusCode = 404;
+
+//   next(new AppError(`Cant find ${req.originalUrl} on this server!`, 404));
 // });
 
-app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
-app.use(globalErrorHandler);
+// Error Handler Middelware (Globl)
+app.use(errorController);
 
-// Test middleware
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  console.log(req.cookies);
-  next();
-});
 module.exports = app;
