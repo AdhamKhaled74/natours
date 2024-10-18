0bae71d170586d9d57faba6b530afa50a7e347a9
AdhamKhaled74 <kadham67153@gmail.com>
2024-10-18 20:25:31 +0300

    vercel deployment again

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,21 +1,53 @@
+// const express = require('express');
+// const bookingController = require('./../controllers/bookingController');
+// const authController = require('./../controllers/authController');
+
+// const router = express.Router();
+// router.use(authController.protect);
+// router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+// router.use(authController.restrictTo('admin', 'lead-guide'));
+
+// router
+//   .route('/')
+//   .get(bookingController.getAllBookings)
+//   .post(bookingController.createBooking);
+
+// router
+//   .route('/:id')
+//   .get(bookingController.getBooking)
+//   .patch(bookingController.updateBooking)
+//   .delete(bookingController.deleteBooking);
+// module.exports = router;
 const express = require('express');
-const bookingController = require('./../controllers/bookingController');
-const authController = require('./../controllers/authController');
 
 const router = express.Router();
-router.use(authController.protect);
-router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 
-router.use(authController.restrictTo('admin', 'lead-guide'));
+const {
+  getCheckoutSession,
+  createBooking,
+  getAllBookings,
+  getBooking,
+  updateBooking,
+  deleteBooking
+} = require('./../controllers/bookingController');
 
+const { protect, restrictTo } = require('./../controllers/authController');
+
+router.use(protect);
+
+router.get('/checkout-session/:tourId', getCheckoutSession);
+
+router.use(restrictTo('admin', 'lead-guide'));
 router
   .route('/')
-  .get(bookingController.getAllBookings)
-  .post(bookingController.createBooking);
+  .get(getAllBookings)
+  .post(createBooking);
 
 router
   .route('/:id')
-  .get(bookingController.getBooking)
-  .patch(bookingController.updateBooking)
-  .delete(bookingController.deleteBooking);
+  .get(getBooking)
+  .patch(updateBooking)
+  .delete(deleteBooking);
+
 module.exports = router;
